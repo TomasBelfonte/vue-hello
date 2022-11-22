f33b703faf7ce1cef00c1a671bb04b85ee7b4275
TomasBelfonte <tomas.belfonte@gmail.com>
2022-11-22 04:53:03 +0100

    Vue-Hello

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ const { createApp } = Vue
             return {
                 message: "Tomas Belfonte",
                 img:"img/20-3670x2462.jpg",
-                classi: "container text-center mt-5 mb-5" 
+                classes: "container text-center mt-5 mb-5" 
             }
         }
     }).mount('#app')
